785ff2b83f9fd9923305b5e30c9f89e29b7a0ffa
nhankun2006 <luongthiennhana2@gmail.com>
2026-03-26 11:49:05 +0700

    fix: handle argv buffer

--- commands/trace.c
+++ commands/trace.c
@@ -1,18 +1,27 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   int mask;
-
-  if(argc < 3){
+  char *p;
+  if (argc < 3) {
     fprintf(2, "usage: trace mask command [args ...]\n");
     exit(1);
   }
+  p = argv[1];
+  if (p[0] == '\0') {
+    fprintf(2, "usage: trace mask command [args ...]\n");
+    exit(1);
+  }
+  for (; *p; p++) {
+    if (*p < '0' || *p > '9') {
+      fprintf(2, "usage: trace mask command [args ...]\n");
+      exit(1);
+    }
+  }
 
   mask = atoi(argv[1]);
-  if(trace(mask) < 0){
+  if (trace(mask) < 0) {
     fprintf(2, "trace: trace failed\n");
     exit(1);
   }

a67099d9edebcd31b5851302387074cbe937d626
nhankun2006 <luongthiennhana2@gmail.com>
2026-03-26 11:29:29 +0700

    feat: trace syscall

---
+++ commands/trace.c
@@ -0,0 +1,23 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int mask;
+
+  if(argc < 3){
+    fprintf(2, "usage: trace mask command [args ...]\n");
+    exit(1);
+  }
+
+  mask = atoi(argv[1]);
+  if(trace(mask) < 0){
+    fprintf(2, "trace: trace failed\n");
+    exit(1);
+  }
+
+  exec(argv[2], &argv[2]);
+  fprintf(2, "trace: exec %s failed\n", argv[2]);
+  exit(1);
+}
